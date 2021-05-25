cc3117336a746459862aea7b2e65e8aad74f7d04
Abdiqani10 <32470515+Abdiqani10@users.noreply.github.com>
2021-05-25 15:52:07 +0200

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://Qani21:'+ encodeURIComponent('Mohamed123456') + '@cluster1.jcvpv.mongodb.net/test?retryWrites=true';
+
+
+module.exports = {
+    mongoURI: dbPassword
+};
